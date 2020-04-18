8fce532f2864714fae836b5fe8e6459454967776
Frank Bennett <biercenator@gmail.com>
2020-04-18 22:51:59 +0900

    Better markings for unfinish cites

--- src/modal.jsx
+++ src/modal.jsx
@@ -96,7 +96,7 @@ export const App = () => {
 
     // Events
     useEffect(() => {
-        console.log('Set listeners =145=');
+        console.log('Set listeners =146=');
         const nodes = document.getElementsByClassName("cite");
         for (var node of nodes) {
             // Pulling details from the event here makes it simpler to
